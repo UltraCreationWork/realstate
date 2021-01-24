1918562108b5bed0056083e8ccd61302a871bc0a
Ishwar jethwani <64900345+UltraCreationWork@users.noreply.github.com>
2021-01-24 13:37:48 +0530

    change in admin

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import website_title,Aboutus,Feature,Services,Projects,Testimonial,Price,FAQ,Artitecture,Contact,Footer
+from .models import website_title,Aboutus,Feature,Services,Projects,Testimonial,Price,FAQ,Artitecture,Contact,Footer,PrivancyPolicy
 
-admin.site.register([website_title,Aboutus,Feature,Services,Projects,Testimonial,Price,FAQ,Artitecture,Contact,Footer])
+admin.site.register([website_title,Aboutus,Feature,Services,Projects,Testimonial,Price,FAQ,Artitecture,Contact,PrivancyPolicy,Footer])
 # Register your models here.
