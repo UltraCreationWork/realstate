8c61676b6b2a25e6745295a5b447ef55470c55f4
Ishwar jethwani <64900345+UltraCreationWork@users.noreply.github.com>
2021-01-24 16:56:50 +0530

    header

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import website_title,Aboutus,Feature,Services,Projects,Testimonial,Price,FAQ,Artitecture,Contact,Footer,PrivancyPolicy
+from .models import website_title,Header,Aboutus,Feature,Services,Projects,Testimonial,Price,FAQ,Artitecture,Contact,Footer,PrivancyPolicy
 
-admin.site.register([website_title,Aboutus,Feature,Services,Projects,Testimonial,Price,FAQ,Artitecture,Contact,PrivancyPolicy,Footer])
+admin.site.register([website_title,Header,Aboutus,Feature,Services,Projects,Testimonial,Price,FAQ,Artitecture,Contact,PrivancyPolicy,Footer])
 # Register your models here.
--- main/views.py
+++ main/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-from .models import website_title
+from .models import ( 
+    website_title,
+    Header,
+    Aboutus,
+    Feature,
+    Services,
+    Projects,
+    Testimonial,
+    Price,
+    FAQ,
+    Artitecture,
+    Contact,
+    Footer,
+    PrivancyPolicy
+    )
+
 
 
 
 def home(request):
     data = {
-        "web_titles":website_title.objects.all()[:1]
+        "web_titles":website_title.objects.all()[:1],
+        "headers":Header.objects.all(),
+
         }
     return render(request,"index.html",data)
     
