416a04a0c1990e68ee70ffbc21caef87d6fa152b
Ishwar jethwani <64900345+UltraCreationWork@users.noreply.github.com>
2021-02-16 09:49:27 +0530

    final

--- main/models.py
+++ main/models.py
@@ -54,8 +54,8 @@ class Header(models.Model):
     additional_img_1 = models.ImageField(upload_to="realestate/property",verbose_name="Side image")
     additional_img_2 = models.ImageField(upload_to="realestate/property",verbose_name="Side image")
     property_video = models.CharField(max_length=2000,verbose_name="Iframe of your property video",blank=True)
-    title = models.CharField(max_length=60,verbose_name="Title")
-    content = models.CharField(max_length=200,verbose_name="Content Box",blank=True)
+    title = models.CharField(max_length=200,verbose_name="Title")
+    content = models.CharField(max_length=500,verbose_name="Content Box",blank=True)
     about_this = RichTextField(verbose_name="About This Project")
     loacation = models.CharField(max_length=2000,verbose_name="Google location of Property",blank=True)
     number = models.PositiveIntegerField(verbose_name="Serial Number")
@@ -80,10 +80,10 @@ class Aboutus(models.Model):
     number = models.PositiveIntegerField(verbose_name="serial Number")
     img_1 = models.ImageField(upload_to="realestate/aboutus",verbose_name="About Header Image")
     img_2 = models.ImageField(upload_to="realestate/aboutus",verbose_name="Image")
-    paragraph = models.CharField(max_length=100,verbose_name="Main Paragrph",blank=True)
-    point_1 = models.CharField(max_length=50,verbose_name="Point one",blank=True)
-    point_2 = models.CharField(max_length=50,verbose_name="Point two",blank=True)
-    point_3 = models.CharField(max_length=50,verbose_name="Point three",blank=True)
+    paragraph = models.CharField(max_length=300,verbose_name="Main Paragrph",blank=True)
+    point_1 = models.CharField(max_length=150,verbose_name="Point one",blank=True)
+    point_2 = models.CharField(max_length=150,verbose_name="Point two",blank=True)
+    point_3 = models.CharField(max_length=150,verbose_name="Point three",blank=True)
     content = RichTextField(verbose_name="About Us Content Box")
 
     def __number__(self):
@@ -98,21 +98,21 @@ class Feature(models.Model):
     title = models.CharField(max_length=100,verbose_name="Feature Name")
     img = models.ImageField(upload_to="realestate/feature",verbose_name="Image of Work")
     paragraph = RichTextField(verbose_name="Feature Discription")
-    point_1 = models.CharField(max_length=50,verbose_name="Point one",blank=True)
-    point_2 = models.CharField(max_length=50,verbose_name="Point two",blank=True)
-    point_3 = models.CharField(max_length=50,verbose_name="Point three",blank=True)
-    point_4 = models.CharField(max_length=50,verbose_name="Point Four",blank=True)
-    point_5 = models.CharField(max_length=50,verbose_name="Point Five",blank=True)
-    point_6 = models.CharField(max_length=50,verbose_name="Point Six",blank=True)
-    point_7 = models.CharField(max_length=50,verbose_name="Point Seven",blank=True)
-    point_8 = models.CharField(max_length=50,verbose_name="Point Eight",blank=True)
-    point_9 = models.CharField(max_length=50,verbose_name="Point Nine",blank=True)
-    point_10 = models.CharField(max_length=50,verbose_name="Point Ten",blank=True)
-    point_11 = models.CharField(max_length=50,verbose_name="Point Eleven",blank=True)
-    point_12 = models.CharField(max_length=50,verbose_name="Point Twele",blank=True)
-    point_13 = models.CharField(max_length=50,verbose_name="Point Thirteen",blank=True)
-    point_14 = models.CharField(max_length=50,verbose_name="Point Fourteen",blank=True)
-    point_15 = models.CharField(max_length=50,verbose_name="Point Fifteen",blank=True)
+    point_1 = models.CharField(max_length=150,verbose_name="Point one",blank=True)
+    point_2 = models.CharField(max_length=150,verbose_name="Point two",blank=True)
+    point_3 = models.CharField(max_length=150,verbose_name="Point three",blank=True)
+    point_4 = models.CharField(max_length=150,verbose_name="Point Four",blank=True)
+    point_5 = models.CharField(max_length=150,verbose_name="Point Five",blank=True)
+    point_6 = models.CharField(max_length=150,verbose_name="Point Six",blank=True)
+    point_7 = models.CharField(max_length=150,verbose_name="Point Seven",blank=True)
+    point_8 = models.CharField(max_length=150,verbose_name="Point Eight",blank=True)
+    point_9 = models.CharField(max_length=150,verbose_name="Point Nine",blank=True)
+    point_10 = models.CharField(max_length=150,verbose_name="Point Ten",blank=True)
+    point_11 = models.CharField(max_length=150,verbose_name="Point Eleven",blank=True)
+    point_12 = models.CharField(max_length=150,verbose_name="Point Twele",blank=True)
+    point_13 = models.CharField(max_length=150,verbose_name="Point Thirteen",blank=True)
+    point_14 = models.CharField(max_length=150,verbose_name="Point Fourteen",blank=True)
+    point_15 = models.CharField(max_length=150,verbose_name="Point Fifteen",blank=True)
 
     def __str__(self):
         return self.title
@@ -174,7 +174,7 @@ class Price(models.Model):
     number = models.PositiveIntegerField(verbose_name=" Serial Number")
     price = models.PositiveIntegerField(verbose_name="service charge")
     parameter = models.CharField(max_length=10,verbose_name="basis of parameter like (month,squre ft,squre meeter etc.)")
-    title = models.CharField(max_length=50,verbose_name="Package Name")
+    title = models.CharField(max_length=200,verbose_name="Package Name")
     service_1 = models.CharField(max_length=100,verbose_name="Service",blank=True)
     service_2 = models.CharField(max_length=100,verbose_name="Service number two",blank=True)
     service_3 = models.CharField(max_length=100,verbose_name="Service number three",blank=True)
@@ -194,7 +194,7 @@ class Price(models.Model):
 
 class FAQ(models.Model):
     number = models.PositiveIntegerField(verbose_name="Question Number")
-    question = models.CharField(max_length=50,verbose_name="Question")
+    question = models.CharField(max_length=250,verbose_name="Question")
     answer = RichTextField(verbose_name="Answer of this question")
 
     def __number__(self):
@@ -217,7 +217,7 @@ class Contact(models.Model):
 
 class PrivacyPolicy(models.Model):
     number = models.PositiveIntegerField(verbose_name="Serial Number")
-    title = models.CharField(max_length=20,verbose_name="Title")
+    title = models.CharField(max_length=120,verbose_name="Title")
     content = RichTextField(verbose_name="Content")
 
     def __str__(self):
@@ -231,8 +231,8 @@ class PrivacyPolicy(models.Model):
 
 
 class Footer(models.Model):
-    title = models.CharField(max_length=10,verbose_name="Company Name")
-    paragraph = models.CharField(max_length=50,verbose_name="Content Box")
+    title = models.CharField(max_length=100,verbose_name="Company Name")
+    paragraph = models.CharField(max_length=150,verbose_name="Content Box")
     link_1 = models.URLField(verbose_name="Twitter link")
     link_2 = models.URLField(verbose_name="Facebook link")
     link_3 = models.URLField(verbose_name="Instagram link")
@@ -267,7 +267,7 @@ class Contact_Detail(models.Model):
 
 
 class Career_header(models.Model):
-    title = models.CharField(max_length=50,verbose_name="image tittle")
+    title = models.CharField(max_length=150,verbose_name="image tittle")
     img = models.ImageField(verbose_name="Image For Career Page")
     date = models.DateField(auto_now_add=True)
     number = models.IntegerField(verbose_name="Serial Number")
@@ -282,7 +282,7 @@ class Career_header(models.Model):
 class Career(models.Model):
     number = models.PositiveIntegerField(verbose_name="Featur Serial Number")
     icon = models.CharField(max_length=23,verbose_name="Icon")
-    title = models.CharField(max_length=100,verbose_name="Post Name")
+    title = models.CharField(max_length=200,verbose_name="Post Name")
     img = models.ImageField(upload_to="realestate/feature",verbose_name="Image of Work")
     paragraph = RichTextField(verbose_name="Post Discription")
     point_1 = models.CharField(max_length=200,verbose_name="heading",blank=True)
@@ -383,9 +383,9 @@ class Current_Projects(models.Model):
 
 
 class Blog(models.Model):
-    title       = models.CharField(max_length=100,verbose_name="title")
+    title       = models.CharField(max_length=200,verbose_name="title")
     blog_image  = models.ImageField(upload_to="blog_photos/",verbose_name="Post Image")
-    overview    = models.CharField(max_length=300,verbose_name="overview")
+    overview    = models.CharField(max_length=500,verbose_name="overview")
     disc        = RichTextField()
     catagory    = models.CharField(choices=catagory,max_length=200,verbose_name="Category")
     date        = models.DateTimeField(auto_now_add=True)
